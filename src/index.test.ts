4a0d9e2273c00f4b19083f82f1abea38ee3ead9b
Kiwi Liu <kiwi71728@icloud.com>
2017-05-30 21:54:51 +0800

    chore: Format codes

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,133 +1,133 @@
 /// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
 import randomString, {
-    _TestEstimatedPoolSize,
-    setRandLetters,
-    LetterInfo,
-    _TestGetLetters,
-    _TestGetRandomIntPool,
-    MAXIMUM_POOL_SIZE
+  _TestEstimatedPoolSize,
+  setRandLetters,
+  LetterInfo,
+  _TestGetLetters,
+  _TestGetRandomIntPool,
+  MAXIMUM_POOL_SIZE
 } from './index'
 import 'should'
 
 const isFinite = require('lodash.isfinite')
 
 interface Elements {
-    [element: string]: number
+  [element: string]: number
 }
 
 class Statistics {
-    private elements: Elements
-    constructor() {
-        this.elements = {}
+  private elements: Elements
+  constructor() {
+    this.elements = {}
+  }
+  public standardDeviation(arr: number[]) {
+    let total = arr.reduce((pre, cur) => pre + cur, 0)
+    let average = 0
+    average = total / arr.length
+    let variation = 0
+    for (let value of arr) {
+      variation += Math.pow(value - average, 2)
     }
-    public standardDeviation( arr: number[] ) {
-        let total = arr.reduce( ( pre, cur ) => pre + cur, 0 )
-        let average = 0
-        average = total / arr.length
-        let variation = 0
-        for( let value of arr ) {
-            variation += Math.pow( value - average, 2 )
-        }
-        return Math.sqrt( variation / arr.length )
+    return Math.sqrt(variation / arr.length)
+  }
+  public setElements(letters: string) {
+    for (const letter of letters.split('')) {
+      if (!this.elements[letter]) {
+        this.elements[letter] = 1
+      }
+      else {
+        this.elements[letter]++
+      }
     }
-    public setElements( letters: string ) {
-        for( const letter of letters.split('') ) {
-            if( !this.elements[ letter ] ) {
-                this.elements[ letter ] = 1
-            }
-            else {
-                this.elements[ letter ]++
-            }
-        }
+  }
+  public getResult(): number {
+    let arr: number[] = []
+    for (const element of Object.keys(this.elements)) {
+      arr.push(this.elements[element])
     }
-    public getResult(): number {
-        let arr: number[] = []
-        for( const element of Object.keys( this.elements ) ) {
-            arr.push( this.elements[ element ] )
-        }
-        return this.standardDeviation( arr )
-    }
+    return this.standardDeviation(arr)
+  }
 }
 
-describe('Validate task named', function() {
+describe('Validate task named', function () {
 
-    describe('setRandLetters', function() {
-        it('should have right number of letters', function() {
-            const { base } = _TestGetLetters()
-            base.should.be.equal(62, 'default collection')
-        })
-        it('should have the same number as the argument passed to setRandLetters', function() {
-            setRandLetters('0123456789')
-            const { base } = _TestGetLetters()
-            base.should.be.equal(10, 'collection: [0-9]')
-        })
+  describe('setRandLetters', function () {
+    it('should have right number of letters', function () {
+      const { base } = _TestGetLetters()
+      base.should.be.equal(62, 'default collection')
     })
-
-    describe('_estimatedPoolSize', function() {
-        it('should return right pool size when letters are [0-9]', function() {
-            setRandLetters('0123456789')
-
-            _TestEstimatedPoolSize( 16, false ).should.be.equal( 2, 'strongCrypto=false' )
-            _TestEstimatedPoolSize( 16, true ).should.be.equal( 4, 'strongCrypto=true' )
-        })
-        it('should return right pool size when letters are default collection', function() {
-            setRandLetters('')
-
-            _TestEstimatedPoolSize( 20, false ).should.be.equal( 3, 'strongCrypto=false' )
-            _TestEstimatedPoolSize( 20, true ).should.be.equal( 5, 'strongCrypto=true' )
-        })
-        it('should return maximum pool size', function() {
-            setRandLetters('')
-            _TestEstimatedPoolSize( 5000, false )
-                .should.be.equal( MAXIMUM_POOL_SIZE, 'strongCrypto=false' )
-            _TestEstimatedPoolSize( 1000, true )
-                .should.be.equal( MAXIMUM_POOL_SIZE, 'strongCrypto=true' )
-        })
+    it('should have the same number as the argument passed to setRandLetters', function () {
+      setRandLetters('0123456789')
+      const { base } = _TestGetLetters()
+      base.should.be.equal(10, 'collection: [0-9]')
     })
+  })
 
-    describe('_getRandomIntPool', function() {
-        it('should return an array and it\'s size is the same as _TestEstimatedPoolSize', function() {
-            _TestGetRandomIntPool( 10, false ).filter( value => isFinite( value ) ).length
-                .should.be.equal( _TestEstimatedPoolSize( 10, false ) )
+  describe('_estimatedPoolSize', function () {
+    it('should return right pool size when letters are [0-9]', function () {
+      setRandLetters('0123456789')
 
-            _TestGetRandomIntPool( 10, true ).filter( value => isFinite( value ) ).length
-                .should.be.equal( _TestEstimatedPoolSize( 10, true ) )
-
-            _TestGetRandomIntPool( 1000, false ).filter( value => isFinite( value ) ).length
-                .should.be.equal( _TestEstimatedPoolSize( 1000, false ) )
-
-            _TestGetRandomIntPool( 1000, true ).filter( value => isFinite( value ) ).length
-                .should.be.equal( _TestEstimatedPoolSize( 1000, true ) )
-        })
+      _TestEstimatedPoolSize(16, false).should.be.equal(2, 'strongCrypto=false')
+      _TestEstimatedPoolSize(16, true).should.be.equal(4, 'strongCrypto=true')
     })
+    it('should return right pool size when letters are default collection', function () {
+      setRandLetters('')
 
-    describe('randomString', function() {
-        it('should return string that the length is specified by the argument', function() {
-            randomString( 10, false ).length.should.be.equal(10)
-            randomString( 10, true ).length.should.be.equal(10)
-            randomString( 50, false ).length.should.be.equal(50)
-            randomString( 50, true ).length.should.be.equal(50)
-            randomString( 1000, false ).length.should.be.equal(1000)
-            randomString( 1000, true ).length.should.be.equal(1000)
-            randomString( 10000, false ).length.should.be.equal(10000)
-            randomString( 10000, true ).length.should.be.equal(10000)
-        })
+      _TestEstimatedPoolSize(20, false).should.be.equal(3, 'strongCrypto=false')
+      _TestEstimatedPoolSize(20, true).should.be.equal(5, 'strongCrypto=true')
     })
+    it('should return maximum pool size', function () {
+      setRandLetters('')
+      _TestEstimatedPoolSize(5000, false)
+        .should.be.equal(MAXIMUM_POOL_SIZE, 'strongCrypto=false')
+      _TestEstimatedPoolSize(1000, true)
+        .should.be.equal(MAXIMUM_POOL_SIZE, 'strongCrypto=true')
+    })
+  })
 
-    describe('statistics', function() {
-        it('should return standard deviation less than 80 when strongCrypto is on', function() {
-            const Statistics1 = new Statistics()
-            for( let i=0; i<10000; ++i ) {
-                Statistics1.setElements( randomString( 30, true ) )
-            }
-            Statistics1.getResult().should.below( 80 )
-        })
-        it('should return standard deviation less than 800 when strongCrypto is off', function() {
-            const Statistics1 = new Statistics()
-            for( let i=0; i<10000; ++i ) {
-                Statistics1.setElements( randomString( 30, false ) )
-            }
-            Statistics1.getResult().should.below( 800 )
-        })
+  describe('_getRandomIntPool', function () {
+    it('should return an array and it\'s size is the same as _TestEstimatedPoolSize', function () {
+      _TestGetRandomIntPool(10, false).filter(value => isFinite(value)).length
+        .should.be.equal(_TestEstimatedPoolSize(10, false))
+
+      _TestGetRandomIntPool(10, true).filter(value => isFinite(value)).length
+        .should.be.equal(_TestEstimatedPoolSize(10, true))
+
+      _TestGetRandomIntPool(1000, false).filter(value => isFinite(value)).length
+        .should.be.equal(_TestEstimatedPoolSize(1000, false))
+
+      _TestGetRandomIntPool(1000, true).filter(value => isFinite(value)).length
+        .should.be.equal(_TestEstimatedPoolSize(1000, true))
     })
+  })
+
+  describe('randomString', function () {
+    it('should return string that the length is specified by the argument', function () {
+      randomString(10, false).length.should.be.equal(10)
+      randomString(10, true).length.should.be.equal(10)
+      randomString(50, false).length.should.be.equal(50)
+      randomString(50, true).length.should.be.equal(50)
+      randomString(1000, false).length.should.be.equal(1000)
+      randomString(1000, true).length.should.be.equal(1000)
+      randomString(10000, false).length.should.be.equal(10000)
+      randomString(10000, true).length.should.be.equal(10000)
+    })
+  })
+
+  describe('statistics', function () {
+    it('should return standard deviation less than 80 when strongCrypto is on', function () {
+      const Statistics1 = new Statistics()
+      for (let i = 0; i < 10000; ++i) {
+        Statistics1.setElements(randomString(30, true))
+      }
+      Statistics1.getResult().should.below(80)
+    })
+    it('should return standard deviation less than 800 when strongCrypto is off', function () {
+      const Statistics1 = new Statistics()
+      for (let i = 0; i < 10000; ++i) {
+        Statistics1.setElements(randomString(30, false))
+      }
+      Statistics1.getResult().should.below(800)
+    })
+  })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 /// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
 
 try {
-    require('babel-polyfill')
+  require('babel-polyfill')
 }
-catch(e) {
-    console.warn(e.message)
+catch (e) {
+  console.warn(e.message)
 }
 
 import * as crypto from 'crypto'
@@ -18,181 +18,176 @@ const ALIGNED_SIZE = 256
 export const MAXIMUM_POOL_SIZE: number = 200
 
 export interface LetterInfo {
-    letters: string
-    base: number
+  letters: string
+  base: number
 }
 
 let customLetters: string = ''
-let alignedLetters: string = LETTERS.repeat( Math.floor( ALIGNED_SIZE / LETTERS.length ) )
+let alignedLetters: string = LETTERS.repeat(Math.floor(ALIGNED_SIZE / LETTERS.length))
 
 function _getLetters(): LetterInfo {
-    const letters: string = ( isString( customLetters ) && customLetters.length )
-        ? customLetters: LETTERS
-    const base: number = letters.length
-    return { letters, base }
+  const letters: string = (isString(customLetters) && customLetters.length)
+    ? customLetters : LETTERS
+  const base: number = letters.length
+  return { letters, base }
 }
 
-export function setRandLetters( argLetters: string ): void {
-    customLetters = argLetters
-    const { letters, base } = _getLetters()
-    alignedLetters = letters.repeat( Math.floor( ALIGNED_SIZE / base ) )
-    if( !alignedLetters.length ) {
-        alignedLetters = letters.substr( 0, ALIGNED_SIZE )
-    }
+export function setRandLetters(argLetters: string): void {
+  customLetters = argLetters
+  const { letters, base } = _getLetters()
+  alignedLetters = letters.repeat(Math.floor(ALIGNED_SIZE / base))
+  if (!alignedLetters.length) {
+    alignedLetters = letters.substr(0, ALIGNED_SIZE)
+  }
 }
 
 export function _TestGetLetters(): LetterInfo {
-    return _getLetters()
+  return _getLetters()
 }
 
 function _estimatedPoolSize(
-    base: number,
-    randStrSize: number,
-    strongCrypto: boolean ): number
-{
-    let poolSize = 0
-    if( strongCrypto ) {
-        poolSize = Math.ceil( randStrSize / 4 )
-    }
-    else {
-        poolSize = Math.ceil( randStrSize * Math.log2( base ) / 53 )
-    }
-    return poolSize > MAXIMUM_POOL_SIZE ? MAXIMUM_POOL_SIZE : poolSize
+  base: number,
+  randStrSize: number,
+  strongCrypto: boolean): number {
+  let poolSize = 0
+  if (strongCrypto) {
+    poolSize = Math.ceil(randStrSize / 4)
+  }
+  else {
+    poolSize = Math.ceil(randStrSize * Math.log2(base) / 53)
+  }
+  return poolSize > MAXIMUM_POOL_SIZE ? MAXIMUM_POOL_SIZE : poolSize
 }
 
 export function _TestEstimatedPoolSize(
-    randStrSize: number,
-    strongCrypto: boolean ): number
-{
-    const { base } = _getLetters()
-    return _estimatedPoolSize( base, randStrSize, strongCrypto )
+  randStrSize: number,
+  strongCrypto: boolean): number {
+  const { base } = _getLetters()
+  return _estimatedPoolSize(base, randStrSize, strongCrypto)
 }
 
 export type RandomIntList = number[]
 
-function _isWindow( obj: Object ): obj is Window {
-    return !!(<Window>obj).document
+function _isWindow(obj: Object): obj is Window {
+  return !!(<Window>obj).document
 }
 
 interface MSWindow extends Window {
-    msCrypto: Pick<Window, 'crypto'>
+  msCrypto: Pick<Window, 'crypto'>
 }
 
-function _getCrypto( obj: Window ): Crypto {
-    return obj.crypto || (<MSWindow>obj).msCrypto
+function _getCrypto(obj: Window): Crypto {
+  return obj.crypto || (<MSWindow>obj).msCrypto
 }
 
 function _strongCryptoSupported(): boolean {
-    if( _isWindow( global ) ) {
-        return !!( _getCrypto( global ) && _getCrypto( global ).getRandomValues )
-    }
-    else {
-        return true
-    }
+  if (_isWindow(global)) {
+    return !!(_getCrypto(global) && _getCrypto(global).getRandomValues)
+  }
+  else {
+    return true
+  }
 }
 
-function _getRandomList( size: number, strongCrypto: boolean ): RandomIntList {
-    const randList = new Array( size )
-    if( strongCrypto && _strongCryptoSupported() ) {
-        if( _isWindow( global ) ) {
-            const tempList = new Uint32Array( size )
-            _getCrypto( global ).getRandomValues( tempList )
-            tempList.forEach( ( value, index ) => {
-                randList[index] = value
-            } )
-            return randList
-        }
-        else {
-            const buffer: Buffer = crypto.randomBytes( size * 4 )
-            for( let i=0; i<size; ++i ) {
-                randList[i] = buffer.readUInt32LE( i * 4 )
-            }
-            return randList
-        }
+function _getRandomList(size: number, strongCrypto: boolean): RandomIntList {
+  const randList = new Array(size)
+  if (strongCrypto && _strongCryptoSupported()) {
+    if (_isWindow(global)) {
+      const tempList = new Uint32Array(size)
+      _getCrypto(global).getRandomValues(tempList)
+      tempList.forEach((value, index) => {
+        randList[index] = value
+      })
+      return randList
     }
     else {
-        let randList: number[] = []
-        for( let i=0; i<size; ++i ) {
-            randList[i] = Math.floor( Math.random() * MAX_INTEGER )
-        }
-        return randList
+      const buffer: Buffer = crypto.randomBytes(size * 4)
+      for (let i = 0; i < size; ++i) {
+        randList[i] = buffer.readUInt32LE(i * 4)
+      }
+      return randList
     }
+  }
+  else {
+    let randList: number[] = []
+    for (let i = 0; i < size; ++i) {
+      randList[i] = Math.floor(Math.random() * MAX_INTEGER)
+    }
+    return randList
+  }
 }
 
 function _getRandomIntPool(
-    base: number,
-    randStrSize: number,
-    strongCrypto: boolean ): RandomIntList
-{
-    const canStrongCrypto = strongCrypto && _strongCryptoSupported()
-    const poolSize: number = _estimatedPoolSize( base, randStrSize, canStrongCrypto )
-    return _getRandomList( poolSize, canStrongCrypto )
+  base: number,
+  randStrSize: number,
+  strongCrypto: boolean): RandomIntList {
+  const canStrongCrypto = strongCrypto && _strongCryptoSupported()
+  const poolSize: number = _estimatedPoolSize(base, randStrSize, canStrongCrypto)
+  return _getRandomList(poolSize, canStrongCrypto)
 }
 
 export function _TestGetRandomIntPool(
-    randStrSize: number,
-    strongCrypto: boolean ): RandomIntList
-{
-    const { base } = _getLetters()
-    return _getRandomIntPool( base, randStrSize, strongCrypto )
+  randStrSize: number,
+  strongCrypto: boolean): RandomIntList {
+  const { base } = _getLetters()
+  return _getRandomIntPool(base, randStrSize, strongCrypto)
 }
 
-function _getAlignedLetters( strongCrypto: boolean ): LetterInfo {
-    const { base, letters } = _getLetters()
-    let newLetters: string = alignedLetters
+function _getAlignedLetters(strongCrypto: boolean): LetterInfo {
+  const { base, letters } = _getLetters()
+  let newLetters: string = alignedLetters
 
-    const remain: number = ALIGNED_SIZE % base
-    if( !remain ) {
-        return { base: ALIGNED_SIZE, letters: newLetters }
-    }
-
-    let randList = _getRandomList( remain, strongCrypto )
-    for( let index = 0; index < remain; ++index ) {
-        newLetters += letters[ randList[index] % base ]
-    }
+  const remain: number = ALIGNED_SIZE % base
+  if (!remain) {
     return { base: ALIGNED_SIZE, letters: newLetters }
+  }
+
+  let randList = _getRandomList(remain, strongCrypto)
+  for (let index = 0; index < remain; ++index) {
+    newLetters += letters[randList[index] % base]
+  }
+  return { base: ALIGNED_SIZE, letters: newLetters }
 }
 
 export default function randomString(
-    strLength: number,
-    strongCrypto: boolean = false): string
-{
-    if ( !isFinite( strLength ) ) return ''
-    let result: string = ''
-    let randNum: number = 0
-    let letterPosition: number = 0
+  strLength: number,
+  strongCrypto: boolean = false): string {
+  if (!isFinite(strLength)) return ''
+  let result: string = ''
+  let randNum: number = 0
+  let letterPosition: number = 0
 
-    if( strongCrypto && _strongCryptoSupported() ) {
-        const { base, letters } = _getAlignedLetters( strongCrypto )
-        let randomNumbers = _getRandomIntPool( base, strLength, strongCrypto )
+  if (strongCrypto && _strongCryptoSupported()) {
+    const { base, letters } = _getAlignedLetters(strongCrypto)
+    let randomNumbers = _getRandomIntPool(base, strLength, strongCrypto)
 
-        while ( result.length < strLength ) {
-            if( !randomNumbers.length ) {
-                randomNumbers = _getRandomIntPool( base, strLength - result.length, strongCrypto )
-            }
-            randNum = <number>randomNumbers.pop()
-            for( let byteOrder = 0; byteOrder < 4; ++byteOrder ) {
-                result += letters[ randNum & 0x000000FF ]
-                randNum = randNum >> 8
-                if( result.length === strLength ) break;
-            }
-        }
+    while (result.length < strLength) {
+      if (!randomNumbers.length) {
+        randomNumbers = _getRandomIntPool(base, strLength - result.length, strongCrypto)
+      }
+      randNum = <number>randomNumbers.pop()
+      for (let byteOrder = 0; byteOrder < 4; ++byteOrder) {
+        result += letters[randNum & 0x000000FF]
+        randNum = randNum >> 8
+        if (result.length === strLength) break;
+      }
     }
-    else {
-        const { base, letters } = _getLetters()
-        let randomNumbers = _getRandomIntPool( base, strLength, strongCrypto )
+  }
+  else {
+    const { base, letters } = _getLetters()
+    let randomNumbers = _getRandomIntPool(base, strLength, strongCrypto)
 
-        while ( result.length < strLength ) {
-            if (randNum < 1) {
-                if( !randomNumbers.length ) {
-                    randomNumbers = _getRandomIntPool( base, strLength - result.length, strongCrypto )
-                }
-                randNum = <number>randomNumbers.pop()
-            }
-            letterPosition = randNum % base
-            randNum = Math.floor(randNum / base)
-            result += letters.charAt(letterPosition)
+    while (result.length < strLength) {
+      if (randNum < 1) {
+        if (!randomNumbers.length) {
+          randomNumbers = _getRandomIntPool(base, strLength - result.length, strongCrypto)
         }
+        randNum = <number>randomNumbers.pop()
+      }
+      letterPosition = randNum % base
+      randNum = Math.floor(randNum / base)
+      result += letters.charAt(letterPosition)
     }
-    return result
+  }
+  return result
 }
